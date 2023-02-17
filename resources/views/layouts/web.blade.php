dcbe4c5a739408ed4f2bde5aa35e64f68b50981e
ReihanFatilla <reihanfatilla@gmail.com>
2023-02-17 11:35:00 +0700

    feat: finished nav menu footer

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -25,7 +25,10 @@
                     <div class="nav_list">
                         @include('layouts.menu')
                     </div>
-                </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
+                </div> <a href="#" class="nav_link nav_profile">
+                    <span class="nav_name">Reihan Fatilla</span>
+                    <i class='bx bx-user-circle nav_icon'></i>
+                </a>
             </nav>
         </div>
         <!--Container Main start-->
