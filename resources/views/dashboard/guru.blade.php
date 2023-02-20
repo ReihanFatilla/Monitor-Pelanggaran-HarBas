0e06fa20bba572205d7a580cfa3ba93c49668eec
ReihanFatilla <reihanfatilla@gmail.com>
2023-02-20 14:25:48 +0700

    feat: finished 4 card and spacer for guru dashboard

--- resources/views/dashboard/guru.blade.php
+++ resources/views/dashboard/guru.blade.php
@@ -2,9 +2,42 @@
 
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/dashboard-guru.css') }}">
-<div class="row">
-    <div class="col-md-3 card-statistic shadow">
-        <div class="row">
+<div class="row justify-content-evenly">
+    <div class="col-md-3">
+        <div class="row card-statistic shadow m-2">
+            <div class="col-md-4 d-flex justify-content-center">
+                <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
+            </div>
+            <div class="col-md-8 center-block">
+                <h5 class="fs-6 text-muted mt-3">Total Pelanggaran</h5>
+                <p class="fs-4 fw-bold">102</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="row card-statistic shadow m-2">
+            <div class="col-md-4 d-flex justify-content-center">
+                <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
+            </div>
+            <div class="col-md-8 center-block">
+                <h5 class="fs-6 text-muted mt-3">Total Pelanggaran</h5>
+                <p class="fs-4 fw-bold">102</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="row card-statistic shadow m-2">
+            <div class="col-md-4 d-flex justify-content-center">
+                <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
+            </div>
+            <div class="col-md-8 center-block">
+                <h5 class="fs-6 text-muted mt-3">Total Pelanggaran</h5>
+                <p class="fs-4 fw-bold">102</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="row card-statistic shadow m-2">
             <div class="col-md-4 d-flex justify-content-center">
                 <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
             </div>
