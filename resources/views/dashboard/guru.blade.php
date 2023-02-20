959924a43c43796c60fcd29822ef080bcfff171a
ReihanFatilla <reihanfatilla@gmail.com>
2023-02-20 14:09:41 +0700

    feat: finished card for guru dashboard

--- resources/views/dashboard/guru.blade.php
+++ resources/views/dashboard/guru.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.web')
 
 @section('content')
-<h1>Statistik Pelanggaran</h1>
+<link rel="stylesheet" href="{{ asset('css/dashboard-guru.css') }}">
 <div class="row">
-    <div class="col-md-3">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4 d-flex justify-content-center">
-                        <i class='bx bx-info-circle nav_icon my-auto bx-lg'></i>
-                    </div>
-                    <div class="col-md-8">
-                        <h5 class="card-text">Total Pelanggaran</h5>
-                        <p class="card-title">102</p>
-                    </div>
-                </div>
+    <div class="col-md-3 card-statistic shadow">
+        <div class="row">
+            <div class="col-md-4 d-flex justify-content-center">
+                <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
+            </div>
+            <div class="col-md-8 center-block">
+                <h5 class="fs-6 text-muted mt-3">Total Pelanggaran</h5>
+                <p class="fs-4 fw-bold">102</p>
             </div>
         </div>
     </div>
-    @endsection
+</div>
+@endsection
