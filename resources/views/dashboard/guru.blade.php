a7dc632bb31f31d36564c98986f73acf2753e47c
ReihanFatilla <reihanfatilla@gmail.com>
2023-02-21 13:39:47 +0700

    feat: added custom color to 4 dashboard statistic

--- resources/views/dashboard/guru.blade.php
+++ resources/views/dashboard/guru.blade.php
@@ -1,49 +1,51 @@
 @extends('layouts.web')
 
 @section('content')
+<p class="fs-4">Statistik Pelanggaran</p>
+
 <link rel="stylesheet" href="{{ asset('css/dashboard-guru.css') }}">
-<div class="row justify-content-evenly">
+<div class="row justify-content-evenly statistic">
     <div class="col-md-3">
-        <div class="row card-statistic shadow m-2">
+        <div class="row card-statistic shadow-lg m-1">
             <div class="col-md-4 d-flex justify-content-center">
                 <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
             </div>
             <div class="col-md-8 center-block">
                 <h5 class="fs-6 text-muted mt-3">Total Pelanggaran</h5>
-                <p class="fs-4 fw-bold">102</p>
+                <p class="fs-5 fw-bold">102</p>
             </div>
         </div>
     </div>
     <div class="col-md-3">
-        <div class="row card-statistic shadow m-2">
+        <div class="row card-statistic shadow-lg m-1">
             <div class="col-md-4 d-flex justify-content-center">
                 <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
             </div>
             <div class="col-md-8 center-block">
                 <h5 class="fs-6 text-muted mt-3">Total Pelanggaran</h5>
-                <p class="fs-4 fw-bold">102</p>
+                <p class="fs-5 fw-bold">102</p>
             </div>
         </div>
     </div>
     <div class="col-md-3">
-        <div class="row card-statistic shadow m-2">
+        <div class="row card-statistic shadow-lg m-1">
             <div class="col-md-4 d-flex justify-content-center">
                 <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
             </div>
             <div class="col-md-8 center-block">
                 <h5 class="fs-6 text-muted mt-3">Total Pelanggaran</h5>
-                <p class="fs-4 fw-bold">102</p>
+                <p class="fs-5 fw-bold">102</p>
             </div>
         </div>
     </div>
     <div class="col-md-3">
-        <div class="row card-statistic shadow m-2">
+        <div class="row card-statistic shadow-lg m-1">
             <div class="col-md-4 d-flex justify-content-center">
                 <i class='bx bx-info-circle nav_icon my-auto bx-md bx-light'></i>
             </div>
             <div class="col-md-8 center-block">
                 <h5 class="fs-6 text-muted mt-3">Total Pelanggaran</h5>
-                <p class="fs-4 fw-bold">102</p>
+                <p class="fs-5 fw-bold">102</p>
             </div>
         </div>
     </div>
