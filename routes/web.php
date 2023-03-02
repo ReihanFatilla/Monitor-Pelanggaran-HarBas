420b1bd70b7d80acbbf39e4db06f357133cabba1
vian <galangdavianp@gmail.com>
2023-03-02 10:04:01 +0700

    slicing welcome

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login.before-login');
+});
+
+Route::get('/welcome', function () {
+    return view('layouts.web');
 });
