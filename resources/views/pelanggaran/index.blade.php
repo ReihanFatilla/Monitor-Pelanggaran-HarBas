389c1ea8cc4a2cf67b3e3cc8fb848a4b55829014
ReihanFatilla <reihanfatilla@gmail.com>
2023-03-06 14:43:59 +0700

    feat: added statistic page with dummy data

--- resources/views/pelanggaran/index.blade.php
+++ resources/views/pelanggaran/index.blade.php
@@ -7,7 +7,7 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="//cdn.datatables.net/1.13.2/js/jquery.dataTables.js"></script>
+<script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.js"></script>
 
 <script>
     var listPelanggaran = function(){
@@ -83,6 +83,7 @@
 
     $(document).ready(function() {
         var pelanggaranTable = $('#pelanggaran-table').DataTable({
+            pagingType: 'simple_numbers',
             data: listPelanggaran(),
             columns: [
                 {
---
+++ resources/views/statistik/index.blade.php
@@ -0,0 +1,161 @@
+@extends('layouts.web')
+
+@section('content')
+<div class="row">
+    <div class="col-md-6 ">
+        <div class="row card-custom shadow-lg m-1 p-3">
+            <p class="fs-6 fw-bold">Pelanggaran Tahun ini</p>
+            <canvas id="pelanggaran-chart-tahun"></canvas>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="row card-custom shadow-lg m-1 p-3">
+            <p class="fs-6 fw-bold">Pelanggar Sesuai Angkatan</p>
+            <canvas id="kelas-chart-minggu"></canvas>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-12 mt-5">
+    <div class="row card-custom shadow-lg m-1 p-3">
+        <p class="fs-6 fw-bold">Pelanggaran Bulan Ini</p>
+        <canvas id="pelanggaran-chart-bulan"></canvas>
+    </div>
+</div>
+
+
+<!-- Chart Js -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const pelanggaranTahunChart = document.getElementById('pelanggaran-chart-tahun');
+
+    const listMonths = ["Jan", "Feb", "Mar", "Apr", "Mei", "Juni", "Juli", "Agus", "Sept", "Nov", "Des"]
+    const listPelanggaran = [50, 40, 35, 67, 63, 23, 24, 53, 14, 19, 20, 28]
+
+
+    new Chart(pelanggaranTahunChart, {
+        type: 'line',
+        data: {
+            labels: listMonths,
+            datasets: [{
+                label: 'Pelanggaran',
+                data: listPelanggaran,
+
+            }],
+            pointStyle: 'circle',
+            pointRadius: 10,
+            pointHoverRadius: 15
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
+
+<script>
+    const kelasMingguChart = document.getElementById('kelas-chart-minggu');
+
+    const listKelas = ["Kelas 10", "Kelas 11", "Kelas 12"];
+
+    const listPelanggaranKelas = [8, 12, 23]
+
+    const listBorderColor = [
+        'rgba(255, 99, 132)',
+        'rgba(255, 159, 64)',
+        'rgba(255, 205, 86)',
+    ]
+
+    new Chart(kelasMingguChart, {
+        type: 'bar',
+        data: {
+            labels: listKelas,
+            datasets: [{
+                label: 'Pelanggaran',
+                data: listPelanggaranKelas,
+                backgroundColor: [
+                    'rgba(255, 99, 132)',
+                    'rgba(255, 159, 64)',
+                    'rgba(255, 205, 86)',
+                ],
+            }],
+            pointStyle: 'circle',
+            pointRadius: 10,
+            pointHoverRadius: 15
+        },
+
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
+
+<script>
+    const pelanggaranBulanChart = document.getElementById('pelanggaran-chart-bulan');
+
+    const listTanggal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+    const listPelanggaranBulan = function(){
+        var listPelanggaran = [];
+        for(var i = 0; i<=30;i++){
+            listPelanggaran.push(Math.floor(Math.random() * 50) + 1)
+        }
+        return listPelanggaran
+    };
+
+    new Chart(pelanggaranBulanChart, {
+        type: 'bar',
+        data: {
+            labels: listTanggal,
+            datasets: [{
+                label: 'Kelas 10',
+                data: listPelanggaranBulan(),
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                ],
+            },{
+                label: 'Kelas 11',
+                data: listPelanggaranBulan(),
+                backgroundColor: [
+                    'rgb(75, 192, 192)',
+                ],
+                borderColor: [
+                    'rgb(75, 192, 192)',
+                ],
+            },{
+                label: 'Kelas 12',
+                data: listPelanggaranBulan(),
+                backgroundColor: [
+                    'rgb(153, 102, 255)'
+                ],
+                borderColor: [
+                    'rgb(153, 102, 255)',
+                ],
+            }
+        ],
+            pointStyle: 'circle',
+            pointRadius: 10,
+            pointHoverRadius: 15
+        },
+
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
+@endsection
