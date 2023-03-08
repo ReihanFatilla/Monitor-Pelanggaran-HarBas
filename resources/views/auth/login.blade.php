58d8f7be7d671f5cda7f15283001d8b7fbd55243
vian <galangdavianp@gmail.com>
2023-03-08 08:08:22 +0700

    login on progress

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <!-- <section class="login" id="login">
+        <div class="login-container">
+
+        </div>
+    </section> -->
+
+    <section class="login" id="login">
+        <div class="container">
+            <div class="row col-md-12">
+                
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
